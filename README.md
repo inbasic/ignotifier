ca1fa39752f89c90fdbcb760890d16057cbc78d8
Eric Caron <eric.caron@gmail.com>
2013-04-28 12:30:55 -0500

    Introducing the ``--sdk`` parameter. Removing embedded libraries so npm can do the heavy lifting.

--- README.md
+++ README.md
@@ -2,26 +2,24 @@
 Multi-account Gmail notifier (without storing passwords locally)
 
 ###General information
-To compile ignotifier project you need to have these softwares installed:
+To compile ignotifier project you need to have these softwares and libraries available:
 * [python](http://www.python.org/getit/)
 * [nodejs](http://nodejs.org/)
+* [Mozilla addon-sdk](https://addons.mozilla.org/en-US/developers/builder)
+  > By default, the addon-sdk folder is assumed to be one directory above the project. This can be modified using the ``--sdk`` parameter.
 
 Folders description:
-* src: ignotifier source code
 * compile: nodejs auto-compiler
-* addon-sdk-*: latest version of [Mozilla addon-sdk](https://addons.mozilla.org/en-US/developers/builder).
 * preview: screenshots
+* src: ignotifier source code
 * template: bootstrap folder
 
 ###How to compile ignotifier
-1. Open a new terminal in the root dir (directory contains src, addon-sdk-*, preview, template, and compile folders)
+1. Open a new terminal in the root dir (directory contains src, preview, template, and compile folders)
 2. Run ``npm install`` to acquire the necessary nodejs packages
-3. **On Windows** Run ``node compile\install.js`` to run ignotifier in a new Firefox profile  
-   To make xpi run ``node compile\install.js --xpi``  
-   For more options use ``node compile\install.js --help``  
-   **On Mac or Linux** Run ``node compile/install.js`` to run ignotifier in a new Firefox profile  
-   To make xpi run ``node compile/install.js --xpi``  
-   For more options use ``node compile/install.js --help``
+3. Run ``node compile/install.js`` to run ignotifier in a new Firefox profile  
+   To make the xpi run ``node compile/install.js --xpi``  
+   For more options use ``node compile/install.js --help``  
 
 ###How to try precompiled latest version
 1. Select the right branch
--- compile/install.js
+++ compile/install.js
@@ -4,6 +4,7 @@ var fs      = require('fs'),
     clc     = require('cli-color'),
     spawn   = require('child_process').spawn,
     exec    = require('child_process').exec;
+
 /** Command Line setup **/
 program
   .version('0.0.1')
@@ -22,9 +23,12 @@ program
     'Show jsConsole in run mode'
   )
   .option('-i, --ip <ip>', 
-    'Send XPI to this ip instead of localhost',
-    String,
-    "localhost"
+    'Send XPI to this host [localhost]',
+    'localhost'
+  )
+  .option('--sdk <sdk path>', 
+    'Path to the Mozilla Add-On SDK library [..]',
+    '..'
   )
   .parse(process.argv);
   
@@ -54,39 +58,52 @@ var installer = function (callback) {
 /** Find SDK **/
 var isWindows = !!process.platform.match(/^win/);
 
-fs.readdir(__dirname + path.sep + "..", function (err, files) {
+fs.readdir(program.sdk, function (err, files) {
   if (err) throw new Error(err);
 
-  files = files.filter(function (file) {
-    return /^addon-sdk-/.test(file);
-  }).sort(function (a, b){
-    var _patern = /(\d+)/g;
-    var temp1 = a.match(_patern), temp2 = b.match(_patern);
-    for (var i = 0; i < 10; i++) {
-      if (temp1[i] != temp2[i]) 
-        return parseInt(temp1[i])<parseInt(temp2[i]);
-    }
-  });
-  if (!files.length) throw new Error("Addon-sdk not found");
-  var curpath = __dirname + path.sep + ".." + path.sep;
-  var bootstrap = curpath + files[0] + "/app-extension/bootstrap.js";
-  var sdk = curpath + files[0] + (isWindows ? "/bin" : "");
+  var actualAddonPath, sdkVersion;
+  /** In case user supplied path pointing to actual SDK directory **/
+  if (/addon-sdk/.test(program.sdk) !== false) {
+    actualAddonPath = program.sdk;
+    sdkVersion = actualAddonPath
+  } else {
+    files = files.filter(function (file) {
+      return /^addon-sdk-/.test(file);
+    }).sort(function (a, b){
+      /** If the directory used has multiple addon-sdk folders, make sure we use the most recent **/
+      var _patern = /(\d+)/g;
+      var temp1 = a.match(_patern), temp2 = b.match(_patern);
+      for (var i = 0; i < 10; i++) {
+        if (temp1[i] != temp2[i]) {
+          return parseInt(temp1[i])<parseInt(temp2[i]);
+        }
+      }
+    });
+    if (!files.length) throw new Error("Addon-sdk not found");
+    actualAddonPath = program.sdk + path.sep + files[0];
+    sdkVersion = files[0];
+  }
+
+  var bootstrap = actualAddonPath + "/app-extension/bootstrap.js";
+  var sdk = actualAddonPath + (isWindows ? "/bin" : "");
   console.log(clc.green(
-    "SDK varsion: " + files[0] + "\n" + 
+    "SDK version: " + sdkVersion + "\n" + 
     "bootstrap found at: " + bootstrap
   ));
+
   /** Replace bootstrap.js **/
   stats = fs.lstatSync('template/');
   if (stats.isDirectory()) {
     fs.createReadStream(bootstrap).pipe(fs.createWriteStream('template/bootstrap.js'));
   }
+
   /** Execute cfx **/
   var cfx = spawn(isWindows ? 'cmd' : 'bash', [], { cwd: sdk });
   if (!isWindows) cfx.stdin.write("echo Bash\n");
 
   var stage = 0;
   cfx.stdout.on('data', function (data) {
-    console.log(clc.gray(data));
+    console.log(clc.xterm(250)(data));
     switch (stage) {
       case 0:
         cfx.stdin.write(isWindows ? "activate\n" : "echo step 1&&source bin/activate\n");
