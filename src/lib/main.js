a4d5ff1a8e311b7ab954a29b08d56400da54f85f
InBasic <inb.cor@gmail.com>
2014-06-14 16:47:50 +0430

    new Promise.all implementation for FF 24.0

--- src/lib/main.js
+++ src/lib/main.js
@@ -110,8 +110,28 @@ try {
   Cu.import("resource://gre/modules/Promise.jsm");
 }
 catch (e) {}
-if (!Promise || !Promise.all) { //Support for FF < 25
-  Promise = require('./q.js').Promise;
+if (!Promise.all) { //Support for FF 24.0
+  Promise = (function () {
+    var obj = {};
+    for (var prop in Promise) {
+       if(Promise.hasOwnProperty(prop)) {
+          obj[prop] = Promise[prop];
+       }
+    }
+    obj.all = function (arr) {
+      var d = new Promise.defer(), results = [], stage = arr.length;
+      function next (succeed, i, result) {
+        results[i] = result;
+        stage -= 1;
+        if (!succeed) d.reject(result);
+        if (!stage) d.resolve(results);
+      }
+      arr.forEach((e, i) => e.then(next.bind(this, true, i), next.bind(this,false, i)));
+      return d.promise;
+    }
+    Object.freeze(obj);
+    return obj;
+  })();
 }
 
 /** Global variables */
