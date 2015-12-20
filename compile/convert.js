8bcb9c3fe57cbbd58a65636057779d5cecbf1fea
InBasic <inb.cor@gmail.com>
2015-12-20 17:47:06 +0330

    updating locales

--- compile/convert.js
+++ compile/convert.js
@@ -3,30 +3,38 @@
 var fs = require('fs');
 
 function convert (input, output) {
-  fs.readFile('src/_locales/' + input + '/messages.json', 'utf8', function (err, data) {
-    if (err) {
-      throw err;
+
+  fs.exists('src/_locales/' + input + '/messages.json', function (exists) {
+    if (exists) {
+      fs.readFile('src/_locales/' + input + '/messages.json', 'utf8', function (err, data) {
+        if (err) {
+          throw err;
+        }
+        var json = JSON.parse(data);
+        var c = '';
+        for (var name in json) {
+          c += name + '=' + json[name].message + '\n';
+        }
+        fs.writeFile('src/locale/' + output + '.properties', c, 'utf8', function (err) {
+          if (err) {
+            throw err;
+          }
+          else {
+            console.log('[done]', input + '/messages.json');
+          }
+        });
+      });
     }
-    var json = JSON.parse(data);
-    var c = '';
-    for (var name in json) {
-      c += name + '=' + json[name].message + '\n';
+    else {
+      console.error('[error]', 'Cannot locate', input + '/messages.json');
     }
-    fs.writeFile('src/locale/' + output + '.properties', c, 'utf8', function (err) {
-      if (err) {
-        throw err;
-      }
-      else {
-        console.log('done');
-      }
-    });
   });
 }
 
 convert('en', 'en');
 convert('el', 'el');
 convert('hu', 'hu');
-//convert('he', 'he');
+convert('he', 'he');
 convert('nl', 'nl');
 convert('pl', 'pl');
 convert('ru', 'ru');
