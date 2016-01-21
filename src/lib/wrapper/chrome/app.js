05624dc78f4ed3488be7f462163ca978f39fab2d
InBasic <inb.cor@gmail.com>
2016-01-21 09:52:58 +0330

    Adding polyfill for 'Promise.defer'

--- src/lib/wrapper/chrome/app.js
+++ src/lib/wrapper/chrome/app.js
@@ -10,6 +10,17 @@ app.once('load', function () {
 });
 
 /* exports */
+if (!Promise.defer) {
+  Promise.defer = function () {
+    let deferred = {};
+    let promise = new Promise(function (resolve, reject) {
+      deferred.resolve = resolve;
+      deferred.reject  = reject;
+    });
+    deferred.promise = promise;
+    return deferred;
+  };
+}
 app.Promise = Promise;
 
 app.button = (function () {
